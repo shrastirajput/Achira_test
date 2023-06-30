7fdd9bea61ab1de6e02817243940d98a34146c1a
shrastirajput <67916357+shrastirajput@users.noreply.github.com>
2023-06-30 16:15:54 +0530

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from machine import Pin, PWM
-from time import sleep
-from sys import stdin, exit
+import machine
+import time
 
+uart = machine.UART(0, baudrate=115200, tx=0, rx=1)  # Use UART 0 with a baud rate of 115200
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=0, rx=1)  # Initialize the UART
 
-pwm_pin = machine.Pin(25)
-pwm = machine.PWM(pwm_pin)
-print("connection")
+led_pin = machine.Pin(25, machine.Pin.OUT)  # Pin 25 for controlling the LED
+pwm = machine.PWM(led_pin)  # Create a PWM object
 
 while True:
-     if input_character = stdin.read(1):
-        self
-        select_result = uselect.select([stdin], [], [], 0)
-        print(input_character)
-        
-        try:
-            value = (input_character stdin.read(1))
-            duty_cycle = value if value >= 0 and value <= 100 else 0  # Limit the value to the valid range (0-100)
-            pwm.duty_u16(int(duty_cycle * 655.35))  # Set the duty cycle of the PWM signal
-        except ValueError:
-            pass  # Ignore invalid data
+    if uart.any():
+        received_data = uart.read(uart.any())  # Read all available data from the UART
+        data_string = received_data.decode().strip()  # Convert bytes to string and remove leading/trailing whitespace
 
-pwm.deinit()
+        try:
+            duty_cycle = int(data_string)  # Convert the received string to an integer
+            if duty_cycle >= 0 and duty_cycle <= 100:  # Make sure the value is within the valid range
+                pwm.duty_u16(int(duty_cycle * 655.35))  # Scale the duty cycle to 16-bit value (0-65535)
+                print("PWM duty cycle set to:", duty_cycle, "%")
+            else:
+                print("Invalid duty cycle value:", duty_cycle)
+        except ValueError:
+            print("Invalid data received")
+
+    time.sleep(0.1)  # Delay to avoid flooding the UART
