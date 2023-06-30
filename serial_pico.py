003d35604240836774742c7d3093c8367f34829d
shrastirajput <67916357+shrastirajput@users.noreply.github.com>
2023-06-30 16:04:59 +0530

    Update serial_pico.py

--- serial_pico.py
+++ serial_pico.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)  # Open the serial connection
+ser = serial.Serial('/dev/ttyACM0', 115200)  # Open the serial connection
 
 duty_cycle = 75  # The duty cycle value (0-100)
 data_to_send = str(duty_cycle)  # Convert the duty cycle to a string
